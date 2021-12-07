6c322c4926f1413e0cf0e9501090c53d21e1a17d
Rebecca Sissman <rsissman15@aol.com>
2021-12-07 16:47:31 -0500

    Completed Assingment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const cards=[];
+
+function writeCards(name,event) {
+    for (let i = 0; i < name.length; i++) {
+      cards.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+      debugger;
+    }
+    return cards;
+  }
+function countDown(i){
+    while(i>=0){
+    console.log(i--);
+    }
+    return i
+}
